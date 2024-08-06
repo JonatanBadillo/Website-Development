f2b8f44c1aedbb191ba0871025fac123f478a0fc
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-06 11:56:32 -0600

    for_combining_decl_and_initl.js

--- Primeros Programas JS/ejemplos1/for_combining/for_combining_decl_and_initl.js
+++ Primeros Programas JS/ejemplos1/for_combining/for_combining_decl_and_initl.js
@@ -1,5 +1,7 @@
 var sum = 0;
+// Declare and initialize the loop variable
 for(var num = 1; num <= 6; num = num + 1) {
+	// Add the current value of num to sum
 	sum = sum + num;
 }
 alert("The sum of the numbers 1 through 6 is " + sum);
