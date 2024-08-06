5af4513a056787239b8edc3a66810520132f811c
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-06 11:44:09 -0600

    embedded_if.js

--- Primeros Programas JS/ejemplos1/embedded_if/embedded_if.js
+++ Primeros Programas JS/ejemplos1/embedded_if/embedded_if.js
@@ -4,7 +4,9 @@ var my_name;
 my_name = prompt("What is your name?");
 my_age = parseInt(prompt("What is your age?"));
 
+// If the user is 18 and their name is Fred
 if(my_age == 18) {
+	// If the user is 18 and their name is Fred
 	if(my_name == "Fred") {
 		alert("Your name is Fred and you are 18!");
 	} 
