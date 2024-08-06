a2de02a520969ec5d63a66cc165f3d5d2b7a4467
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-06 12:01:33 -0600

    while_example.js

--- Primeros Programas JS/ejemplos1/while_example/while_example.js
+++ Primeros Programas JS/ejemplos1/while_example/while_example.js
@@ -1,6 +1,7 @@
 var answer;
-
+// Keep asking the user for the first book in the Bible until they get it right
 while(answer != "Genesis") {
 	answer = prompt("What is the first book in the Bible?");
 }
+// Alert the user that they got the answer right
 alert("You got the answer right!");
