8fc876c5600777d9763516512151a339251d3a55
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-06 11:41:33 -0600

    combining conditions

--- Primeros Programas JS/ejemplos1/combining_conditions/combining_conditions.js
+++ Primeros Programas JS/ejemplos1/combining_conditions/combining_conditions.js
@@ -1,9 +1,12 @@
 var my_age;
 var my_name;
 
+// Ask the user for their name and age
 my_name = prompt("What is your name?");
+// Convert the age to a number
 my_age = parseInt(prompt("What is your age?"));
 
+// If the user is 18 and their name is Fred
 if(my_age == 18 && my_name == "Fred") {
 	alert("Your name is Fred and you are 18!");
 }
