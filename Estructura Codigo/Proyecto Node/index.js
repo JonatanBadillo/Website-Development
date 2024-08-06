d5dedfefe4d8e4adb2de376a1e53dc2c851c92b1
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-05 20:44:49 -0600

    index.js file

--- Estructura Codigo/Proyecto Node/index.js
+++ Estructura Codigo/Proyecto Node/index.js
@@ -1,2 +1,9 @@
-console.log ("¡Hola mundo!");
-console.log ("¡Adiós Mundo!");
+console.log("¡Hola mundo!");
+console.log("¡Adiós Mundo!");
+
+console.log(5 + 6);
+
+let myVariable = 5;
+if (myVariable === 5) {
+  console.log("The variable is 5!");
+}
