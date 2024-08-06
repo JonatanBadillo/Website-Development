0a137d7094f7efa76e9da884880e2f47a6b861cf
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-06 11:26:46 -0600

    feat: add age calculation and display in 20 years

--- Primeros Programas JS/Simple Program JS/application.js
+++ Primeros Programas JS/Simple Program JS/application.js
@@ -2,9 +2,12 @@ var person_age_string;
 var person_age_number;
 var age_in_twenty_years;
 
+// Recibe la edad de la persona como String
 person_age_string = prompt("What is your age?");
+//  Convierte la edad de la persona a un número
 person_age_number = parseInt(person_age_string);
+// Calcula la edad de la persona en 20 años
 age_in_twenty_years = person_age_number + 20;
-
+// Muestra la edad de la persona en 20 años
 alert("In 20 years you will be " + age_in_twenty_years + 
 	" years old");
