8be02d428bc98588d0b05f3e730512c229f3e477
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-08 11:45:24 -0600

    global and local variables

--- Mas Ejemplos JS/Global and Local Variables/global_and_local_variables.js
+++ Mas Ejemplos JS/Global and Local Variables/global_and_local_variables.js
@@ -1,10 +1,15 @@
 var myvar = 3; // Global variable
 
+// This function has a local variable called myvar
 var my_function = function() {
 	var myvar = 5; // This version of myvar is now *local*
 };
-
+// Call the global variable
 alert("myvar = " + myvar);
+
+// Call the function my_function
 my_function();
+
+// Display the value of myvar
 alert("After calling my_function, myvar = " + myvar); 
 
