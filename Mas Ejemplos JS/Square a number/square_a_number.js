d1db8aadb90fa701afc9e895b25f07c347bc00e5
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-08 11:57:07 -0600

    square a number

--- Mas Ejemplos JS/Square a number/square_a_number.js
+++ Mas Ejemplos JS/Square a number/square_a_number.js
@@ -1,7 +1,9 @@
+// This function takes a number and returns the square of that number.
 var square_a_number = function(num) {
 	var value = num * num;
 	return value;
 };
-
+// Call the function square_a_number
 alert("The square of 3 is " + square_a_number(3));
+// Call the function square_a_number
 alert("The square of 4 is " + square_a_number(4));
