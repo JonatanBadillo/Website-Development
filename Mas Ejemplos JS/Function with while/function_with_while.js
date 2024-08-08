d786b731cce6f9fa5a984af63ecfaeb5bfd81781
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-08 11:24:35 -0600

    function with for loop and user input

--- Mas Ejemplos JS/Function with while/function_with_while.js
+++ Mas Ejemplos JS/Function with while/function_with_while.js
@@ -1,3 +1,4 @@
+// Ejemplo de función con for
 var sum_range = function(range_start, range_end) {
 	var sum = 0;
 	for(var num = range_start; num <= range_end; num = num + 1) {
@@ -6,7 +7,13 @@ var sum_range = function(range_start, range_end) {
 	return sum;
 };
 
+// The parseInt function is used to convert the string to an integer
 var start_val = parseInt(prompt("Enter the first number"));
+// The parseInt function is used to convert the string to an integer
 var end_val = parseInt(prompt("Enter the last number"));
+
+// Call the function sum_range
 var result = sum_range(start_val, end_val);
+
+// Display the result
 alert("The sum of all of the numbers between " + start_val + " and " + end_val + " is " + result);
