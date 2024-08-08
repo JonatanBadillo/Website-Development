24373fb33ff97a49699a01ae8e665e4b87f82abd
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-08 11:17:38 -0600

    function_calling_function

--- Mas Ejemplos JS/Function calling function/function_calling_function.js
+++ Mas Ejemplos JS/Function calling function/function_calling_function.js
@@ -7,12 +7,14 @@ var square_a_number = function(num) {
 
 
 // This function returns the sum of squares of numbers between range_start and range_end
-var sum_squares_for_range = function(range_start, range_end) {
+var sum_squares_for_range = function(range_start, range_end) { 
 	var sum = 0;
 	for(var num = range_start; num <= range_end; num = num + 1) {
 		sum = sum + square_a_number(num);
 	}
 	return sum;
 };
+
+// Call the function sum_squares_for_range
 alert("The sum of squares between 2 and 5 is " + sum_squares_for_range(2, 5));
 
