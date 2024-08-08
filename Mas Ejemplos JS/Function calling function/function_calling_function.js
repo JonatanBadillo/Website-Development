186b5960d7686866a90a32c7abffd15cb8408ed8
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-08 11:16:34 -0600

    function calling a function

--- Mas Ejemplos JS/Function calling function/function_calling_function.js
+++ Mas Ejemplos JS/Function calling function/function_calling_function.js
@@ -1,8 +1,12 @@
+// 2. Function calling function
 var square_a_number = function(num) {
+	// This function returns the square of a number
 	var value = num * num;
 	return value;
 };
 
+
+// This function returns the sum of squares of numbers between range_start and range_end
 var sum_squares_for_range = function(range_start, range_end) {
 	var sum = 0;
 	for(var num = range_start; num <= range_end; num = num + 1) {
