9f37bc6f7d7bf9634cb9cfe5b98928aa706cb667
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-09 07:24:50 -0600

    array.length

--- Introduccion Objetos y Arreglos/index.js
+++ Introduccion Objetos y Arreglos/index.js
@@ -13,3 +13,20 @@ myArr = [ "banana", "apple", "squid", "cake", "pear" ]
 console.log(myArr[0]) // shows "banana"
 console.log(myArr[2]) // shows "apple"
 console.log(myArr[3]) // shows "squid"
+
+
+// Obtener la longitud de un arreglo
+// Para obtener el tamaño de un arreglo determinado, usamos el método .length directamente en ese arreglo.
+// Intentemos poner la longitud de MyArray en una nueva variable llamada arrayLength.
+// Cuando usamos la console log, mostrará 4:
+myArr = [ "one", 2, "three", { "value": "four" } ]
+let arrayLength = myArr.length
+console.log(arrayLength) // shows 4
+
+
+
+
+
+
+
+
