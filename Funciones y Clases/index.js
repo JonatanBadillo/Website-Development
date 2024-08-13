f73781e36f919d1d6b07876457b924a59f04e008
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-12 19:59:18 -0600

    chore: Agregar ejemplos de pilas y referencias en memoria

--- Funciones y Clases/index.js
+++ Funciones y Clases/index.js
@@ -17,3 +17,20 @@ function words(word1, word2) {
 console.log(words("Hello", "John")); // Hello John
 console.log(words("Hello", "Jake")); // Hello Jake
 console.log(words("Good bye", "Alice")); // Good bye Alice
+
+// Nota: Las funciones son de tipo objeto, por lo que cuando las declaramos, sus datos se
+// almacenan en el heap. Sin embargo, llamarlas las agregará a la pila de llamadas como
+// variables.
+
+
+// Ejecutar argumentos con los “tres puntos”
+// Los argumentos también se pueden llamar en una función a través de un arreglo, utilizando
+// la sintaxis de tres puntos, que cubrimos anteriormente cuando observamos cómo fusionar
+// arreglos y objetos. Veamos cómo funciona eso usando nuestro ejemplo anterior:
+function words(word1, word2) {
+return word1 + " " + word2
+}
+let validWords = [ "Hello", "John" ]
+console.log(words(...validWords)) // Hello John
+// Esto es realmente útil en el mundo real, donde a menudo tenemos datos almacenados en
+// arreglos y objetos, que luego queremos pasar a las funciones.
