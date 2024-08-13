cfd4b17beaa01f1b256cfe3318c3a6f1ccd379e3
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-13 12:05:18 -0600

    html manipulation

--- WebPages/example_1/multiply_two_text_fields_full.js
+++ WebPages/example_1/multiply_two_text_fields_full.js
@@ -1,11 +1,20 @@
+// This script multiplies two text fields and displays the result in a span element.
 var multiply_fields = function() {
+	// Get the values from the text fields
 	var fld1 = document.getElementById("field1");
 	var fld2 = document.getElementById("field2");
+
+	// Convert the values to integers
 	var val1 = parseInt(fld1.value);
 	var val2 = parseInt(fld2.value);
+	// Multiply the values
 	var result = val1 * val2;
+	// Get the span element
 	var results_span = document.getElementById("results");
+	// Display the result
 	results_span.textContent = result;
 };
+// Get the button element
 var btn = document.getElementById("my_button");
+// Attach the function to the button
 btn.onclick = multiply_fields;
