58d6645b1ae75301510a1a6ca292f3bb533cbb1c
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-12 19:47:31 -0600

    primera funcion

---
+++ Funciones y Clases/index.js
@@ -0,0 +1,11 @@
+// Las funciones y clases
+// son lo que usamos para envolver ciertas piezas de funcionalidad en
+// bloques reutilizables. Al usar una función o clase, podemos repetir tareas específicas muchas
+// veces sin tener que reescribir el código.
+
+// En el siguiente ejemplo, creamos una función simple
+// que devuelve "Hello World" al ejecutar:
+function myFunction() {
+  return "Hello World";
+}
+console.log(myFunction());
