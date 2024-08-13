aeb74ca96cbb92ce9f3602c892fa07461063dd73
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-13 11:32:42 -0600

    // funcion que recibe dos objetos y los compara

--- Objetos Arreglos/function two objects/function_two_objects.js
+++ Objetos Arreglos/function two objects/function_two_objects.js
@@ -1,8 +1,13 @@
+// funcion que recibe dos objetos y los compara
 var process_transaction = function(account, transaction) {
+	// si la cuenta es igual a la cuenta de origen de la transaccion
 	if(account.account_number == transaction.from_account) {
+		// se resta el monto de la transaccion al saldo actual de la cuenta
 		account.current_balance = account.current_balance - transaction.amount;
 	} else {
+		// si la cuenta es igual a la cuenta de destino de la transaccion
 		if(account.account_number == transaction.to_account) {
+			// se suma el monto de la transaccion al saldo actual de la cuenta
 			account.current_balance = account.current_balance + transaction.amount;
 		} else {
 			// Do nothing
