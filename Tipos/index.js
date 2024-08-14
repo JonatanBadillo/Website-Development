097b3735af30ceed586d795c2b612eaf044b9644
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-13 20:00:31 -0600

    feat: Add String.prototype.at() method examples and explanations

--- Tipos/index.js
+++ Tipos/index.js
@@ -15,9 +15,11 @@ console.log(String.prototype) // 'number'
 // tipo:
 let someVariable = 'string'
 someVariable.at(1) // 't'
-console.log(someVariable.at(1)) // 't'
 
 
+let str = "Hello, World!";
+console.log(str.at(0));  // H
+console.log(str.at(-1)); // !
 
 
 // También puedes llamar a los number methods en un número utilizando dos puntos, donde el
@@ -29,3 +31,31 @@ console.log(someVariable.at(1)) // 't'
 
 // Los métodos number también se pueden llamar a través de Number.toString ().
 Number.toString(5) // '5'
+
+// el método estático keys() se pueden encontrar en el envoltorio de objetos
+// y se puede llamar para obtener las llaves de cualquier objeto:
+Object.keys({ "hello" : "world", "goodbye" : "world" })
+// [ "hello", "goodbye" ]
+
+
+// Para resumir lo que hemos discutido hasta ahora:
+// 1. Existen varios tipos primitivos en JavaScript que no tienen métodos ni propiedades
+// de forma predeterminada. También son inmutables. Estas son cosas como cadena,
+// número y booleano.
+
+// 2. Todos los tipos en JavaScript, incluidas primitivas y objetos, heredan métodos de
+// objetos de envoltura. Entonces, todos los datos del tipo cadena heredarán todos los
+// métodos que se encuentran en String.prototype.
+
+// 3. Si necesitas averiguar qué métodos puedes usar en cualquier tipo de datos en
+// JavaScript, puedes buscarlo en Google... o puedes usar en console.log el prototipo de
+// envoltura para verlos a todos como console.log (String.prototype).
+
+// 4. Los métodos encontrados en el nivel superior del prototipo de un objeto de envoltorio
+// (es decir, String.prototype.at()) pueden llamarse directamente en cualquier cosa de
+// ese tipo, por ejemplo, 'string'.at (2).
+
+// 5. Existen métodos estáticos en la mayoría de los objetos de envoltura. Por lo general,
+// se encuentran en una función de constructor de ese envoltorio, es decir,
+// Number.constructor.MAX_SAFE_INTEGER, y deben llamarse a través del objeto
+// de envoltorio, como Object.keys() o Object.values().
