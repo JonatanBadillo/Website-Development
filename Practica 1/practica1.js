288844d375fabf54f57efc42edab293bc2a4616e
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-15 11:20:22 -0600

    problema 2, de la practica 1

--- Practica 1/practica1.js
+++ Practica 1/practica1.js
@@ -14,3 +14,17 @@ for(i=0;i<myArray.length;i++){
     console.log(myArray[i]);
 }
 
+
+// 2. ¿Cómo se crea un arreglo multidimensional? 
+// Problema 
+// Deseas realizar un seguimiento del contenido de una tabla, en la que una dimensión  representa las filas y la otra las columnas. 
+// Solución 
+// No existe ninguna sintaxis especial para crear un arreglo multidimensional. Para ello, debes agregar un nuevo arreglo dentro de un elemento de otro arreglo. También puedes crear un arreglo multidimensional con literales de arreglo. 
+// creacion de un arreglo multidimensional
+let myArray2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+// imprimir en consola el primer elemento de la primera fila
+console.log(myArray2[0][0]);
+// imprimir en consola el segundo elemento de la segunda fila
+console.log(myArray2[1][1]);
+
+
