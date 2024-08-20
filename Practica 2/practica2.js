044e796fe76029d4498eb815822f73b377eb5eac
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-20 11:14:51 -0600

    practica 2, ejercicio 1

--- Practica 2/practica2.js
+++ Practica 2/practica2.js
@@ -3,3 +3,11 @@
 // ¿En qué situación utilizarías un objeto de conjunto en lugar de un arreglo? 
 // Solución 
 // Existen algunas similitudes entre los dos objetos. Ambos pueden contener datos de diferentes  tipos. Lo que diferencia a un conjunto es que todos los valores deben ser únicos. 
+
+// La diferencia entre conjuntos y arreglos es que no los conjuntos no pueden tener elementos duplicados.
+let mySet = new Set();
+mySet.add(2);
+mySet.add(3);
+mySet.add(3);
+mySet.add(4);
+console.log(mySet);
