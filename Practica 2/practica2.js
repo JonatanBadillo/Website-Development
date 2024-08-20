bf44604137fb9dfdcb0681d7883b54c51d37789d
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-20 11:09:44 -0600

    practica 2

---
+++ Practica 2/practica2.js
@@ -0,0 +1,5 @@
+// 1. ¿Cuál es la diferencia entre un conjunto y un arreglo? 
+// Problema 
+// ¿En qué situación utilizarías un objeto de conjunto en lugar de un arreglo? 
+// Solución 
+// Existen algunas similitudes entre los dos objetos. Ambos pueden contener datos de diferentes  tipos. Lo que diferencia a un conjunto es que todos los valores deben ser únicos. 
