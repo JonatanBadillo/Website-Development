a14e5386aee3212fb21675b79de2bde01ff19322
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-27 11:34:04 -0600

    myShell.js

---
+++ Primera Practica NodeJS/myShell.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+function greet(name) {
+... console.log(`Hello $(name)`);
+... }; greet('World');
+function greet(name) {... console.log(`Hello $(name)`);... }; greet('World');
+function greet(name) {console.log(`Hello $(name)`); }; greet('World');
+const sayHello = 'Hello World';
+console.log(sayHello);
+function greet(){
+console.log('Hello World!');
