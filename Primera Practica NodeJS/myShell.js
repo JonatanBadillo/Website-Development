2dd29430d29709aff232178e0f2b4c32a63c89cf
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-30 07:22:57 -0600

    require http

--- Primera Practica NodeJS/myShell.js
+++ Primera Practica NodeJS/myShell.js
@@ -1,8 +1,8 @@
-import { createServer } from "http";
+const { createServer } = require("http");
+
 const server = createServer();
 server.listen(8080, () => {
   console.log(
-    `Server is listening to
-http://localhost:${server.address().port}`
+    `Server is listening to http://localhost:${server.address().port}`
   );
 });
