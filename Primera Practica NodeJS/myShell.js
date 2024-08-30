56e79206a23e6fb8f8317720e425e07e7907ed06
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-30 07:29:31 -0600

    Add server functionality to myShell.js

--- Primera Practica NodeJS/myShell.js
+++ Primera Practica NodeJS/myShell.js
@@ -1,8 +1,15 @@
 const { createServer } = require("http");
 
-const server = createServer();
+// Crear el servidor HTTP
+const server = createServer((request, response) => {
+  response.writeHead(200, { "content-type": "text/plain; charset=utf-8" });
+  response.write("Hello ");
+  response.end(" World\n");
+});
+
+// Escuchar en el puerto 8080
 server.listen(8080, () => {
   console.log(
-    `Server is listening to http://localhost:${server.address().port}`
+    `Server is listening to\n\nhttp://localhost:${server.address().port}`
   );
 });
