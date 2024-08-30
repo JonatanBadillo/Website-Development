c989b28c79c1677ca192009b01158d58f352b25b
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-08-30 07:53:12 -0600

    Update respuesta.js to include server functionality

---
+++ Primera Practica NodeJS/respuesta.js
@@ -0,0 +1,26 @@
+const { createServer } = require("http");
+
+const server = createServer((request, response) => {
+    response.writeHead(200, { "content-type": "text/html; charset=utf-8" });
+    const url = new URL(request.url, "http://localhost:8080");
+    const body = `<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>Node.js Demo</title>
+</head>
+<body>
+<h1 style="color:green">Hello ${url.searchParams.get("name")}
+</h1>
+</body>
+</html>`;
+    response.end(body);
+});
+
+// Inicia el servidor y escucha en el puerto 8080
+server.listen(8080, () => {
+    console.log(
+        `Server is listening to http://localhost:${server.address().port}`
+    );
+});
