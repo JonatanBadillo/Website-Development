53554141355b848ebf715623fa56664d1bc75567
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-01 11:06:01 -0600

    server  index.php

---
+++ Servidor Web PHP/index.php
@@ -0,0 +1,9 @@
+<?php
+// Mostrar errores (solo para desarrollo)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Página principal
+echo "¡Hola, mundo! Este es mi servidor PHP";
+?>
