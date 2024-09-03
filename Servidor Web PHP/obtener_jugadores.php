34f6c9654cf7e6acb4c2752a318d3f662d0b395c
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-02 19:40:35 -0600

    Actualizar consulta SQL para obtener todos los jugadores en obtener_jugadores.php

--- Servidor Web PHP/obtener_jugadores.php
+++ Servidor Web PHP/obtener_jugadores.php
@@ -1,4 +1,7 @@
 <?php
+// SQL para seleccionar todos los jugadores
 $sql = "SELECT * FROM jugadores";
+
+// ejecutar consulta y guardar el resultado en la variable $result
 $result = $connection->query($sql);
 ?>
