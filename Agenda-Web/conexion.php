9c4e273c05f1ce2cbe1f2b24aece4863d5ca4cc3
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-04 18:49:39 -0600

    chore: Update database connection and error handling in conexion.php

--- Agenda-Web/conexion.php
+++ Agenda-Web/conexion.php
@@ -1,5 +1,8 @@
 <?php
+// establecer la conexión con la base de datos
 $conexion = new mysqli("127.0.0.1", "root", "", "contactos_db");
+
+// verificar si hay algún error en la conexión
 if ($conexion->connect_error) {
     die("Error en la conexión: " . $conexion->connect_error);
 }
