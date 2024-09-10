5779d4794be228ed6ddbab2b215d2268d854fde5
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-10 12:49:10 -0600

    chore: Actualizar manejo de archivos en el controlador de solicitudes HTTP

--- Práctica 2 de Node.js/webapp/dist/handler.js
+++ Práctica 2 de Node.js/webapp/dist/handler.js
@@ -1,22 +1,17 @@
 "use strict";
+// import { handler } from './handler';
 // import { IncomingMessage, ServerResponse } from "http";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
-const fs_1 = require("fs");
-// Definición de la función handler
+//import { readFile } from "fs";
+const promises_1 = require("fs/promises");
 const handler = (req, res) => {
-    // Lee el archivo "data.json"
-    (0, fs_1.readFile)("data.json", (err, data) => {
-        if (err == null) {
-            // Envía los datos del archivo como respuesta
-            res.end(data, () => console.log("Archivo enviado"));
-        }
-        else {
-            // En caso de error, muestra el mensaje de error y establece el código de estado 500
-            console.log(`Error: ${err.message}`);
-            res.statusCode = 500;
-            res.end();
-        }
+    const p = (0, promises_1.readFile)("data.json");
+    p.then((data) => res.end(data, () => console.log("File sent")));
+    p.catch((err) => {
+        console.log(`Error: ${err.message}`);
+        res.statusCode = 500;
+        res.end();
     });
 };
 exports.handler = handler;
--- Práctica 2 de Node.js/webapp/src/handler.ts
+++ Práctica 2 de Node.js/webapp/src/handler.ts
@@ -1,3 +1,4 @@
+// import { handler } from './handler';
 // import { IncomingMessage, ServerResponse } from "http";
 
 // // Definición de la función handler
@@ -6,21 +7,34 @@
 //     res.end("Hello World");
 // }
 
-// Importa los módulos necesarios
+// // Importa los módulos necesarios
+// import { IncomingMessage, ServerResponse } from "http";
+// import { readFile } from "fs";
+// // Definición de la función handler
+// export const handler = (req: IncomingMessage, res: ServerResponse) => {
+//     // Lee el archivo "data.json"
+//     readFile("data.json", (err: Error | null, data: Buffer) => {
+//         if (err == null) {
+//             // Envía los datos del archivo como respuesta
+//             res.end(data, () => console.log("Archivo enviado"));
+//         } else {
+//             // En caso de error, muestra el mensaje de error y establece el código de estado 500
+//             console.log(`Error: ${err.message}`);
+//             res.statusCode = 500;
+//             res.end();
+//         }
+//     });
+// };
+
 import { IncomingMessage, ServerResponse } from "http";
-import { readFile } from "fs";
-// Definición de la función handler
+//import { readFile } from "fs";
+import { readFile } from "fs/promises";
 export const handler = (req: IncomingMessage, res: ServerResponse) => {
-    // Lee el archivo "data.json"
-    readFile("data.json", (err: Error | null, data: Buffer) => {
-        if (err == null) {
-            // Envía los datos del archivo como respuesta
-            res.end(data, () => console.log("Archivo enviado"));
-        } else {
-            // En caso de error, muestra el mensaje de error y establece el código de estado 500
-            console.log(`Error: ${err.message}`);
-            res.statusCode = 500;
-            res.end();
-        }
-    });
+  const p: Promise<Buffer> = readFile("data.json");
+  p.then((data: Buffer) => res.end(data, () => console.log("File sent")));
+  p.catch((err: Error) => {
+    console.log(`Error: ${err.message}`);
+    res.statusCode = 500;
+    res.end();
+  });
 };
