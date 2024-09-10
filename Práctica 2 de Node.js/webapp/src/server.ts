dac9d022b9df1d13daccedcf2f508f76088ec23b
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-09 18:14:07 -0600

    chore: Actualizar archivo server.ts para crear un servidor HTTP en el puerto especificado

--- Práctica 2 de Node.js/webapp/src/server.ts
+++ Práctica 2 de Node.js/webapp/src/server.ts
@@ -1,14 +1,14 @@
 // crea un servidor HTTP y escucha en el puerto especificado.
-import { handler } from "./handler";
-import { createServer } from "http";
+import { handler } from "./handler"; // se importa manejador de solicitudes
+import { createServer } from "http"; // se importa la funcion para crear servidor
 
 // puerto en el que se va a ejecutar el servidor
 const port = 5000;
 
-// crear el servidor HTTP
+// crear el servidor HTTP, con el manejador importado para crear servidor http
 const server = createServer(handler);
 
-// escuchar en el puerto especificado
+// inicia el servidor : escuchando en el puerto especificado y muestre mensaje en consola
 server.listen(port, function(){
     console.log(`Servidor escuchando en el puerto ${port}`);
 });
