790a4429306b5631b7285a794ab89fde380220ff
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-09 19:43:22 -0600

    chore: Actualizar manejo de archivos en el controlador de solicitudes HTTP

--- Práctica 2 de Node.js/webapp/src/handler.ts
+++ Práctica 2 de Node.js/webapp/src/handler.ts
@@ -1,7 +1,26 @@
-import { IncomingMessage, ServerResponse } from "http";
+// import { IncomingMessage, ServerResponse } from "http";
 
+// // Definición de la función handler
+// export const handler = (req: IncomingMessage, res: ServerResponse) => {
+//     // Envía la respuesta "Hello World"
+//     res.end("Hello World");
+// }
+
+// Importa los módulos necesarios
+import { IncomingMessage, ServerResponse } from "http";
+import { readFile } from "fs";
 // Definición de la función handler
 export const handler = (req: IncomingMessage, res: ServerResponse) => {
-    // Envía la respuesta "Hello World"
-    res.end("Hello World");
-}
+    // Lee el archivo "data.json"
+    readFile("data.json", (err: Error | null, data: Buffer) => {
+        if (err == null) {
+            // Envía los datos del archivo como respuesta
+            res.end(data, () => console.log("Archivo enviado"));
+        } else {
+            // En caso de error, muestra el mensaje de error y establece el código de estado 500
+            console.log(`Error: ${err.message}`);
+            res.statusCode = 500;
+            res.end();
+        }
+    });
+};
