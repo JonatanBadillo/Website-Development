adb89c4a681d7a194f97cd2a9c01d469f825e4ab
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-10 18:19:03 -0600

    chore: Update server.js and server.ts to create an HTTP server on the specified port

--- Práctica 2 de Node.js/webapp/dist/server.js
+++ Práctica 2 de Node.js/webapp/dist/server.js
@@ -1,13 +1,22 @@
 "use strict";
+// // crea un servidor HTTP y escucha en el puerto especificado.
+// import { handler } from "./handler"; // se importa manejador de solicitudes
+// import { createServer } from "http"; // se importa la funcion para crear servidor
 Object.defineProperty(exports, "__esModule", { value: true });
-// crea un servidor HTTP y escucha en el puerto especificado.
-const handler_1 = require("./handler"); // se importa manejador de solicitudes
-const http_1 = require("http"); // se importa la funcion para crear servidor
-// puerto en el que se va a ejecutar el servidor
+// // puerto en el que se va a ejecutar el servidor
+// const port = 5000;
+// // crear el servidor HTTP, con el manejador importado para crear servidor http
+// const server = createServer(handler);
+// // inicia el servidor : escuchando en el puerto especificado y muestre mensaje en consola
+// server.listen(port, function(){
+//     console.log(`Servidor escuchando en el puerto ${port}`);
+// });
+const http_1 = require("http");
+const handler_1 = require("./handler");
 const port = 5000;
-// crear el servidor HTTP, con el manejador importado para crear servidor http
-const server = (0, http_1.createServer)(handler_1.handler);
-// inicia el servidor : escuchando en el puerto especificado y muestre mensaje en consola
-server.listen(port, function () {
-    console.log(`Servidor escuchando en el puerto ${port}`);
+const server = (0, http_1.createServer)();
+server.on("request", handler_1.handler);
+server.listen(port);
+server.on("listening", () => {
+    console.log(`(Event) Server listening on port ${port}`);
 });
--- Práctica 2 de Node.js/webapp/src/server.ts
+++ Práctica 2 de Node.js/webapp/src/server.ts
@@ -1,14 +1,24 @@
-// crea un servidor HTTP y escucha en el puerto especificado.
-import { handler } from "./handler"; // se importa manejador de solicitudes
-import { createServer } from "http"; // se importa la funcion para crear servidor
+// // crea un servidor HTTP y escucha en el puerto especificado.
+// import { handler } from "./handler"; // se importa manejador de solicitudes
+// import { createServer } from "http"; // se importa la funcion para crear servidor
 
-// puerto en el que se va a ejecutar el servidor
+// // puerto en el que se va a ejecutar el servidor
+// const port = 5000;
+
+// // crear el servidor HTTP, con el manejador importado para crear servidor http
+// const server = createServer(handler);
+
+// // inicia el servidor : escuchando en el puerto especificado y muestre mensaje en consola
+// server.listen(port, function(){
+//     console.log(`Servidor escuchando en el puerto ${port}`);
+// });
+
+import { createServer } from "http";
+import { handler } from "./handler";
 const port = 5000;
-
-// crear el servidor HTTP, con el manejador importado para crear servidor http
-const server = createServer(handler);
-
-// inicia el servidor : escuchando en el puerto especificado y muestre mensaje en consola
-server.listen(port, function(){
-    console.log(`Servidor escuchando en el puerto ${port}`);
+const server = createServer();
+server.on("request", handler);
+server.listen(port);
+server.on("listening", () => {
+  console.log(`(Event) Server listening on port ${port}`);
 });
