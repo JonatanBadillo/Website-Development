bd18c5c062feb6e4b2b6e22d3443dea199299f92
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-10 20:08:21 -0600

    chore: Update request handler to use fs/promises for file reading and add writePromise

--- Práctica 2 de Node.js/webapp/dist/promises.js
+++ Práctica 2 de Node.js/webapp/dist/promises.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.endPromise = void 0;
+exports.writePromise = exports.endPromise = void 0;
 const http_1 = require("http");
 const util_1 = require("util");
 // Importamos la interfaz ServerResponse del módulo http
@@ -9,3 +9,7 @@ const util_1 = require("util");
 // aplicada al método end de la clase ServerResponse. También especificamos el tipo
 // de dato que espera recibir como argumento (data) y el tipo de dato que devuelve (Promise<void>)
 exports.endPromise = (0, util_1.promisify)(http_1.ServerResponse.prototype.end);
+// Exportamos una constante llamada writePromise que es igual a la función promisify
+// aplicada al método write de la clase ServerResponse. También especificamos el tipo
+// de dato que espera recibir como argumento (data) y el tipo de dato que devuelve (Promise<void>)
+exports.writePromise = (0, util_1.promisify)(http_1.ServerResponse.prototype.write);
--- Práctica 2 de Node.js/webapp/src/promises.ts
+++ Práctica 2 de Node.js/webapp/src/promises.ts
@@ -9,3 +9,10 @@ import { promisify } from "util";
 // aplicada al método end de la clase ServerResponse. También especificamos el tipo
 // de dato que espera recibir como argumento (data) y el tipo de dato que devuelve (Promise<void>)
 export const endPromise = promisify(ServerResponse.prototype.end) as (data: any) => Promise<void>;
+
+
+// Exportamos una constante llamada writePromise que es igual a la función promisify
+// aplicada al método write de la clase ServerResponse. También especificamos el tipo
+// de dato que espera recibir como argumento (data) y el tipo de dato que devuelve (Promise<void>)
+export const writePromise = promisify(ServerResponse.prototype.write) as
+(data: any) => Promise<void>;
