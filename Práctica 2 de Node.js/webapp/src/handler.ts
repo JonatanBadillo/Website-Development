7ff6f1c5ecebd69e52915b07ca956d25fa8c7123
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-10 18:53:58 -0600

    chore: Update request handler to use fs/promises for file reading

--- Práctica 2 de Node.js/webapp/src/handler.ts
+++ Práctica 2 de Node.js/webapp/src/handler.ts
@@ -26,24 +26,48 @@
 //     });
 // };
 
+
+// import { IncomingMessage, ServerResponse } from "http";
+// import { readFile } from "fs/promises";
+// // Definición de la función handler
+// export const handler = (req: IncomingMessage, res: ServerResponse) => {
+//   // Lee el archivo "data.json" usando fs/promises
+//   // promesa que producirá un objeto Buffer cuando se complete su operación asíncrona.
+//   const p: Promise<Buffer> = readFile("data.json");
+
+//   // Maneja la promesa para enviar los datos del archivo como respuesta
+//   // El método then se utiliza para registrar la función que se invocará si se resuelve la promesa
+//   p.then((data: Buffer) => res.end(data, () => console.log("Archivo enviado")));
+
+//   // Maneja el error en caso de que ocurra
+//   // Una promesa rechazada es aquella en la que se ha producido un error. El método catch se
+//   // utiliza para registrar una función que maneja el error producido por una promesa rechazada
+//   p.catch((err: Error) => {
+//     console.log(`Error: ${err.message}`);
+//     res.statusCode = 500;
+//     res.end();
+//   });
+// };
+
+// Importa los módulos necesarios
 import { IncomingMessage, ServerResponse } from "http";
 import { readFile } from "fs/promises";
 // Definición de la función handler
 export const handler = (req: IncomingMessage, res: ServerResponse) => {
-  // Lee el archivo "data.json" usando fs/promises
-  // promesa que producirá un objeto Buffer cuando se complete su operación asíncrona.
-  const p: Promise<Buffer> = readFile("data.json");
+    // Lee el archivo "data.json" usando fs/promises
+    // promesa que producirá un objeto Buffer cuando se complete su operación asíncrona.
+    const p: Promise<Buffer> = readFile("data.json");
 
-  // Maneja la promesa para enviar los datos del archivo como respuesta
-  // El método then se utiliza para registrar la función que se invocará si se resuelve la promesa
-  p.then((data: Buffer) => res.end(data, () => console.log("Archivo enviado")));
+    // Maneja la promesa para enviar los datos del archivo como respuesta
+    // El método then se utiliza para registrar la función que se invocará si se resuelve la promesa
+    p.then((data: Buffer) => res.end(data, () => console.log("Archivo enviado")));
 
-  // Maneja el error en caso de que ocurra
-  // Una promesa rechazada es aquella en la que se ha producido un error. El método catch se
-  // utiliza para registrar una función que maneja el error producido por una promesa rechazada
-  p.catch((err: Error) => {
-    console.log(`Error: ${err.message}`);
-    res.statusCode = 500;
-    res.end();
-  });
+    // Maneja el error en caso de que ocurra
+    // Una promesa rechazada es aquella en la que se ha producido un error. El método catch se
+    // utiliza para registrar una función que maneja el error producido por una promesa rechazada
+    p.catch((err: Error) => {
+        console.log(`Error: ${err.message}`);
+        res.statusCode = 500;
+        res.end();
+    });
 };
