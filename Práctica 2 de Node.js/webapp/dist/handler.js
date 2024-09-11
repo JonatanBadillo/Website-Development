1c04bc7c909c198ef06a90747f7ae0c809c93043
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-10 18:59:04 -0600

    chore: Update request handler to use async/await for file reading

--- Práctica 2 de Node.js/webapp/dist/handler.js
+++ Práctica 2 de Node.js/webapp/dist/handler.js
@@ -5,20 +5,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
 const promises_1 = require("fs/promises");
 // Definición de la función handler
-const handler = (req, res) => {
+const handler = async (req, res) => {
     // Lee el archivo "data.json" usando fs/promises
     // promesa que producirá un objeto Buffer cuando se complete su operación asíncrona.
-    const p = (0, promises_1.readFile)("data.json");
-    // Maneja la promesa para enviar los datos del archivo como respuesta
-    // El método then se utiliza para registrar la función que se invocará si se resuelve la promesa
-    p.then((data) => res.end(data, () => console.log("Archivo enviado")));
-    // Maneja el error en caso de que ocurra
-    // Una promesa rechazada es aquella en la que se ha producido un error. El método catch se
-    // utiliza para registrar una función que maneja el error producido por una promesa rechazada
-    p.catch((err) => {
-        console.log(`Error: ${err.message}`);
-        res.statusCode = 500;
-        res.end();
-    });
+    const data = await (0, promises_1.readFile)("data.json");
+    // Envía los datos del archivo como respuesta
+    res.end(data, () => console.log("Archivo enviado"));
 };
 exports.handler = handler;
--- Práctica 2 de Node.js/webapp/src/handler.ts
+++ Práctica 2 de Node.js/webapp/src/handler.ts
@@ -49,25 +49,39 @@
 //   });
 // };
 
-// Importa los módulos necesarios
+// // Importa los módulos necesarios
+// import { IncomingMessage, ServerResponse } from "http";
+// import { readFile } from "fs/promises";
+// // Definición de la función handler
+// export const handler = (req: IncomingMessage, res: ServerResponse) => {
+//     // Lee el archivo "data.json" usando fs/promises
+//     // promesa que producirá un objeto Buffer cuando se complete su operación asíncrona.
+//     const p: Promise<Buffer> = readFile("data.json");
+
+//     // Maneja la promesa para enviar los datos del archivo como respuesta
+//     // El método then se utiliza para registrar la función que se invocará si se resuelve la promesa
+//     p.then((data: Buffer) => res.end(data, () => console.log("Archivo enviado")));
+
+//     // Maneja el error en caso de que ocurra
+//     // Una promesa rechazada es aquella en la que se ha producido un error. El método catch se
+//     // utiliza para registrar una función que maneja el error producido por una promesa rechazada
+//     p.catch((err: Error) => {
+//         console.log(`Error: ${err.message}`);
+//         res.statusCode = 500;
+//         res.end();
+//     });
+// };
+
+// Uso de las palabras clave async y await en el archivo handler.ts en la carpeta src.
 import { IncomingMessage, ServerResponse } from "http";
 import { readFile } from "fs/promises";
+
 // Definición de la función handler
-export const handler = (req: IncomingMessage, res: ServerResponse) => {
+export const handler = async (req: IncomingMessage, res: ServerResponse) => {
     // Lee el archivo "data.json" usando fs/promises
     // promesa que producirá un objeto Buffer cuando se complete su operación asíncrona.
-    const p: Promise<Buffer> = readFile("data.json");
+    const data: Buffer = await readFile("data.json");
 
-    // Maneja la promesa para enviar los datos del archivo como respuesta
-    // El método then se utiliza para registrar la función que se invocará si se resuelve la promesa
-    p.then((data: Buffer) => res.end(data, () => console.log("Archivo enviado")));
-
-    // Maneja el error en caso de que ocurra
-    // Una promesa rechazada es aquella en la que se ha producido un error. El método catch se
-    // utiliza para registrar una función que maneja el error producido por una promesa rechazada
-    p.catch((err: Error) => {
-        console.log(`Error: ${err.message}`);
-        res.statusCode = 500;
-        res.end();
-    });
+    // Envía los datos del archivo como respuesta
+    res.end(data, () => console.log("Archivo enviado"));
 };
