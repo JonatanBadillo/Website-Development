358c733f681091b28f96ff3c353ebf6de05996ef
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-11 18:49:32 -0600

    chore: Refactorizar controlador de solicitudes HTTP para iterar de forma asíncrona

--- Práctica 2 de Node.js/webapp/dist/handler.js
+++ Práctica 2 de Node.js/webapp/dist/handler.js
@@ -4,8 +4,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
 const promises_1 = require("./promises");
-//import { Worker } from "worker_threads";
-const counter_cb_1 = require("./counter_cb");
+//import { Count } from "./counter_cb";
+const count_promise_1 = require("./count_promise");
+//import { Count } from "./counter_cb";
 // Declaración de variables
 const total = 2000000000;
 const iterations = 5;
@@ -14,24 +15,21 @@ let shared_counter = 0;
 const handler = async (req, res) => {
     // Incrementa el contador compartido
     const request = shared_counter++;
-    // Llama a la función Count para realizar el trabajo de las solicitudes
-    (0, counter_cb_1.Count)(request, iterations, total, async (err, update) => {
-        if (err !== null) {
-            // En caso de error, muestra el mensaje de error, establece el código de estado 500 y finaliza la respuesta
-            console.log(err);
-            res.statusCode = 500;
-            await res.end();
-        }
-        else if (update !== true) {
-            // Si no hay error y no es la última iteración, muestra el mensaje en la consola y escribe el mensaje en la respuesta
-            const msg = `Request: ${request}, Iteration: ${(update)}`;
-            console.log(msg);
-            await promises_1.writePromise.bind(res)(msg + "\n");
-        }
-        else {
-            // Si no hay error y es la última iteración, finaliza la respuesta
-            await promises_1.endPromise.bind(res)("Done");
-        }
-    });
+    try {
+        // Llama a la función Count para realizar el trabajo de las solicitudes
+        await (0, count_promise_1.Count)(request, iterations, total);
+        // Mensaje a imprimir en cada iteración
+        const msg = `Request: ${request}, Iterations: ${(iterations)}`;
+        // Escribe el mensaje en la respuesta
+        await promises_1.writePromise.bind(res)(msg + "\n");
+        // Finaliza la respuesta
+        await promises_1.endPromise.bind(res)("Done");
+    }
+    catch (err) {
+        // En caso de error, muestra el mensaje de error, establece el código de estado 500 y finaliza la respuesta
+        console.log(err);
+        res.statusCode = 500;
+        res.end();
+    }
 };
 exports.handler = handler;
--- Práctica 2 de Node.js/webapp/src/handler.ts
+++ Práctica 2 de Node.js/webapp/src/handler.ts
@@ -247,10 +247,54 @@
 
 
 
+// import { IncomingMessage, ServerResponse } from "http";
+// import { endPromise, writePromise } from "./promises";
+// //import { Worker } from "worker_threads";
+// import { Count } from "./counter_cb";
+// // Declaración de variables
+// const total = 2_000_000_000;
+// const iterations = 5;
+// let shared_counter = 0;
+
+// // Definición de la función handler
+// export const handler = async (req: IncomingMessage, res: ServerResponse) => {
+//     // Incrementa el contador compartido
+//     const request = shared_counter++;
+
+//     // Llama a la función Count para realizar el trabajo de las solicitudes
+//     Count(request, iterations, total, async (err, update) => {
+//         if (err !== null) {
+//             // En caso de error, muestra el mensaje de error, establece el código de estado 500 y finaliza la respuesta
+//             console.log(err);
+//             res.statusCode = 500;
+//             await res.end();
+//         } else if (update !== true) {
+//             // Si no hay error y no es la última iteración, muestra el mensaje en la consola y escribe el mensaje en la respuesta
+//             const msg = `Request: ${request}, Iteration: ${(update)}`;
+//             console.log(msg);
+//             await writePromise.bind(res)(msg + "\n");
+//         } else {
+//             // Si no hay error y es la última iteración, finaliza la respuesta
+//             await endPromise.bind(res)("Done");
+//         }
+//     });
+// };
+
+
+
+
+
+
+
+
+// La diferencia importante con los ejemplos anteriores es que el trabajo para las solicitudes se
+// realiza en paralelo, en lugar de que todo el trabajo se realice en un solo hilo. Esto se logra al crear un nuevo subproceso de trabajo para cada solicitud.
 import { IncomingMessage, ServerResponse } from "http";
 import { endPromise, writePromise } from "./promises";
-//import { Worker } from "worker_threads";
-import { Count } from "./counter_cb";
+//import { Count } from "./counter_cb";
+import { Count } from "./count_promise";
+//import { Count } from "./counter_cb";
+
 // Declaración de variables
 const total = 2_000_000_000;
 const iterations = 5;
@@ -261,21 +305,22 @@ export const handler = async (req: IncomingMessage, res: ServerResponse) => {
     // Incrementa el contador compartido
     const request = shared_counter++;
 
-    // Llama a la función Count para realizar el trabajo de las solicitudes
-    Count(request, iterations, total, async (err, update) => {
-        if (err !== null) {
-            // En caso de error, muestra el mensaje de error, establece el código de estado 500 y finaliza la respuesta
-            console.log(err);
-            res.statusCode = 500;
-            await res.end();
-        } else if (update !== true) {
-            // Si no hay error y no es la última iteración, muestra el mensaje en la consola y escribe el mensaje en la respuesta
-            const msg = `Request: ${request}, Iteration: ${(update)}`;
-            console.log(msg);
-            await writePromise.bind(res)(msg + "\n");
-        } else {
-            // Si no hay error y es la última iteración, finaliza la respuesta
-            await endPromise.bind(res)("Done");
-        }
-    });
+    try {
+        // Llama a la función Count para realizar el trabajo de las solicitudes
+        await Count(request, iterations, total);
+
+        // Mensaje a imprimir en cada iteración
+        const msg = `Request: ${request}, Iterations: ${(iterations)}`;
+
+        // Escribe el mensaje en la respuesta
+        await writePromise.bind(res)(msg + "\n");
+
+        // Finaliza la respuesta
+        await endPromise.bind(res)("Done");
+    } catch (err: any) {
+        // En caso de error, muestra el mensaje de error, establece el código de estado 500 y finaliza la respuesta
+        console.log(err);
+        res.statusCode = 500;
+        res.end();
+    }
 };
