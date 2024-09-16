49d34562e74217f83802996aa40ba4e068dd092e
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-16 12:54:15 -0600

    Refactorizar controlador de solicitudes HTTP para agregar soporte HTTPS

--- Práctica 2 de Node.js/webapp/dist/handler.js
+++ Práctica 2 de Node.js/webapp/dist/handler.js
@@ -2,32 +2,40 @@
 // import { handler } from './handler';
 // import { IncomingMessage, ServerResponse } from "http";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
+exports.handler = exports.isHttps = void 0;
+const tls_1 = require("tls");
 const url_1 = require("url");
-// Definición de la función handler
-const handler = async (req, resp) => {
-    // Parsea la URL de la solicitud y muestra sus componentes en la consola
-    const parsedURL = new url_1.URL(req.url ?? "", `http://${req.headers.host}`);
-    // Verifica si el método de la solicitud no es GET o si la ruta es "/favicon.ico"
+// Función para verificar si la solicitud es HTTPS
+const isHttps = (req) => {
+    return req.socket instanceof tls_1.TLSSocket && req.socket.encrypted;
+};
+exports.isHttps = isHttps;
+// Función de controlador para manejar la solicitud
+const handler = (req, resp) => {
+    // Verificar el protocolo de la solicitud (HTTP o HTTPS)
+    const protocol = (0, exports.isHttps)(req) ? "https" : "http";
+    // Parsear la URL de la solicitud
+    const parsedURL = new url_1.URL(req.url ?? "", `${protocol}://${req.headers.host}`);
+    // Verificar si la solicitud no es GET o si la ruta es "/favicon.ico"
     if (req.method !== "GET" || parsedURL.pathname == "/favicon.ico") {
-        // Si no cumple las condiciones, responde con un código 404 y termina la respuesta
+        // Responder con un código 404 y terminar la respuesta
         resp.writeHead(404, "Not Found");
         resp.end();
         return;
     }
     else {
-        // Si cumple las condiciones, responde con un código 200 y continúa con la generación de la respuesta
+        // Responder con un código 200 y continuar con la generación de la respuesta
         resp.writeHead(200, "OK");
-        // Verifica si la URL de la solicitud tiene el parámetro "keyword"
+        // Verificar si la URL de la solicitud tiene el parámetro "keyword"
         if (!parsedURL.searchParams.has("keyword")) {
-            // Si no tiene el parámetro "keyword", escribe "Hello, HTTP" en la respuesta
-            resp.write("Hello, HTTP");
+            // Si no tiene el parámetro "keyword", escribir "Hello, HTTP" en la respuesta
+            resp.write(`Hello, ${protocol.toUpperCase()}`);
         }
         else {
-            // Si tiene el parámetro "keyword", escribe "Hello, " seguido del valor del parámetro en la respuesta
+            // Si tiene el parámetro "keyword", escribir "Hello, " seguido del valor del parámetro en la respuesta
             resp.write(`Hello, ${parsedURL.searchParams.get("keyword")}`);
         }
-        // Termina la respuesta
+        // Terminar la respuesta
         resp.end();
         return;
     }
