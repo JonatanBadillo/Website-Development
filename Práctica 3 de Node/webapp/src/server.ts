15019359f848d575c4bc9446045f9b2ee99bb2f6
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-17 20:32:33 -0600

    Refactorizar controlador de solicitudes HTTP para agregar soporte HTTPS

--- Práctica 3 de Node/webapp/src/server.ts
+++ Práctica 3 de Node/webapp/src/server.ts
@@ -1,8 +1,26 @@
-import { createServer } from "http";
-import { handler } from "./handler";
-const port = 5000;
+import express, { Express } from "express";  
+import { redirectionHandler, greetHandler } from "./handler";  
+import { createServer as createHttpsServer } from "https";  
+import { readFileSync } from "fs";  
 
-const server = createServer(handler);
-server.listen(port, function() {
- console.log(`Server listening on port ${port}`);
-});
+const app: Express = express();  
+const port = 5000;  
+const https_port = 5500;  
+
+// Manejo de redirección para HTTP  
+app.get("*", redirectionHandler);  
+
+// Crear servidor HTTPS  
+const httpsConfig = {  
+    key: readFileSync("key.pem"),  
+    cert: readFileSync("cert.pem")  
+};  
+
+const httpsApp: Express = express();  
+httpsApp.get("/greet/:name?", greetHandler); // Ruta con parámetro opcional  
+
+const httpsServer = createHttpsServer(httpsConfig, httpsApp);  
+httpsServer.listen(https_port, () => console.log(`HTTPS Server listening on port ${https_port}`));  
+
+// Iniciar el servidor HTTP  
+app.listen(port, () => console.log(`(Event) Server listening on port ${port}`));
