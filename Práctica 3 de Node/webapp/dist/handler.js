d350e2160066ea9334cbe032e0fff80e50011dc3
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-17 20:39:32 -0600

    feat: Agregar controladores de redireccionamiento y saludo

--- Práctica 3 de Node/webapp/dist/handler.js
+++ Práctica 3 de Node/webapp/dist/handler.js
@@ -1,7 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
-const handler = (req, res) => {
-    res.end("Hello World");
+exports.greetHandler = exports.redirectionHandler = void 0;
+const redirectionHandler = (req, res) => {
+    res.redirect(302, "https://localhost:5500");
 };
-exports.handler = handler;
+exports.redirectionHandler = redirectionHandler;
+const greetHandler = (req, res) => {
+    const name = req.params.name; // Obtiene el parámetro de ruta  
+    const greeting = name ? `Hello, ${name}!` : "Hello, stranger!";
+    res.status(200).send(greeting);
+};
+exports.greetHandler = greetHandler;
--- Práctica 3 de Node/webapp/dist/server.js
+++ Práctica 3 de Node/webapp/dist/server.js
@@ -1,9 +1,25 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const http_1 = require("http");
+const express_1 = __importDefault(require("express"));
 const handler_1 = require("./handler");
+const https_1 = require("https");
+const fs_1 = require("fs");
+const app = (0, express_1.default)();
 const port = 5000;
-const server = (0, http_1.createServer)(handler_1.handler);
-server.listen(port, function () {
-    console.log(`Server listening on port ${port}`);
-});
+const https_port = 5500;
+// Manejo de redirección para HTTP  
+app.get("*", handler_1.redirectionHandler);
+// Crear servidor HTTPS  
+const httpsConfig = {
+    key: (0, fs_1.readFileSync)("key.pem"),
+    cert: (0, fs_1.readFileSync)("cert.pem")
+};
+const httpsApp = (0, express_1.default)();
+httpsApp.get("/greet/:name?", handler_1.greetHandler); // Ruta con parámetro opcional  
+const httpsServer = (0, https_1.createServer)(httpsConfig, httpsApp);
+httpsServer.listen(https_port, () => console.log(`HTTPS Server listening on port ${https_port}`));
+// Iniciar el servidor HTTP  
+app.listen(port, () => console.log(`(Event) Server listening on port ${port}`));
