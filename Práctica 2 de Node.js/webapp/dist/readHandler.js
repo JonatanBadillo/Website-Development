c5cceb92afff2a63c7e4c0bcd83aa397a279a24e
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-22 15:15:01 -0600

    feat: Mejorar manejo de lectura de datos en el controlador básico

--- Práctica 2 de Node.js/webapp/dist/readHandler.js
+++ Práctica 2 de Node.js/webapp/dist/readHandler.js
@@ -1,8 +1,23 @@
 "use strict";
+// import { IncomingMessage, ServerResponse } from "http";
+// export const readHandler = (req: IncomingMessage, resp: ServerResponse) => {
+// // TODO - read request body
+// resp.end();
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.readHandler = void 0;
+// Exportamos la función readHandler que manejará la lectura de datos
 const readHandler = (req, resp) => {
-    // TODO - read request body
-    resp.end();
+    // Establecemos la codificación de la solicitud como "utf-8"
+    req.setEncoding("utf-8");
+    // Escuchamos el evento "data" para leer los datos de la solicitud
+    req.on("data", (data) => {
+        console.log(data); // Imprimimos los datos en la consola
+    });
+    // Escuchamos el evento "end" para indicar que se han leído todos los datos
+    req.on("end", () => {
+        console.log("End: all data read"); // Imprimimos un mensaje indicando que se han leído todos los datos
+        resp.end(); // Finalizamos la respuesta
+    });
 };
 exports.readHandler = readHandler;
--- Práctica 2 de Node.js/webapp/src/readHandler.ts
+++ Práctica 2 de Node.js/webapp/src/readHandler.ts
@@ -1,5 +1,25 @@
+// import { IncomingMessage, ServerResponse } from "http";
+// export const readHandler = (req: IncomingMessage, resp: ServerResponse) => {
+// // TODO - read request body
+// resp.end();
+// }
+
+// Lectura de datos en el archivo readHandler.ts en la carpeta src.
 import { IncomingMessage, ServerResponse } from "http";
+
+// Exportamos la función readHandler que manejará la lectura de datos
 export const readHandler = (req: IncomingMessage, resp: ServerResponse) => {
-// TODO - read request body
-resp.end();
-}
+    // Establecemos la codificación de la solicitud como "utf-8"
+    req.setEncoding("utf-8");
+    
+    // Escuchamos el evento "data" para leer los datos de la solicitud
+    req.on("data", (data: string) => {
+        console.log(data); // Imprimimos los datos en la consola
+    });
+    
+    // Escuchamos el evento "end" para indicar que se han leído todos los datos
+    req.on("end", () => {
+        console.log("End: all data read"); // Imprimimos un mensaje indicando que se han leído todos los datos
+        resp.end(); // Finalizamos la respuesta
+    });
+};
