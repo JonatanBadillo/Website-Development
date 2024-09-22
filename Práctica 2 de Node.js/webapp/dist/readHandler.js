dea42980cb3b66f63aaa582eb12b2110c7721f23
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-22 15:29:08 -0600

    Refactorizar controlador de lectura para utilizar bucle asíncrono en lugar de eventos

--- Práctica 2 de Node.js/webapp/dist/readHandler.js
+++ Práctica 2 de Node.js/webapp/dist/readHandler.js
@@ -6,18 +6,15 @@
 // }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.readHandler = void 0;
-// Exportamos la función readHandler que manejará la lectura de datos
-const readHandler = (req, resp) => {
+// Definimos la función readHandler como una función asíncrona que manejará la lectura de datos
+const readHandler = async (req, resp) => {
     // Establecemos la codificación de la solicitud como "utf-8"
     req.setEncoding("utf-8");
-    // Escuchamos el evento "data" para leer los datos de la solicitud
-    req.on("data", (data) => {
+    // Iteramos de forma asíncrona sobre los datos de la solicitud
+    for await (const data of req) {
         console.log(data); // Imprimimos los datos en la consola
-    });
-    // Escuchamos el evento "end" para indicar que se han leído todos los datos
-    req.on("end", () => {
-        console.log("End: all data read"); // Imprimimos un mensaje indicando que se han leído todos los datos
-        resp.end(); // Finalizamos la respuesta
-    });
+    }
+    console.log("End: all data read"); // Imprimimos un mensaje indicando que se han leído todos los datos
+    resp.end(); // Finalizamos la respuesta
 };
 exports.readHandler = readHandler;
--- Práctica 2 de Node.js/webapp/src/readHandler.ts
+++ Práctica 2 de Node.js/webapp/src/readHandler.ts
@@ -4,22 +4,42 @@
 // resp.end();
 // }
 
-// Lectura de datos en el archivo readHandler.ts en la carpeta src.
-import { IncomingMessage, ServerResponse } from "http";
+// // Lectura de datos en el archivo readHandler.ts en la carpeta src.
+// import { IncomingMessage, ServerResponse } from "http";
 
-// Exportamos la función readHandler que manejará la lectura de datos
-export const readHandler = (req: IncomingMessage, resp: ServerResponse) => {
+// // Exportamos la función readHandler que manejará la lectura de datos
+// export const readHandler = (req: IncomingMessage, resp: ServerResponse) => {
+//     // Establecemos la codificación de la solicitud como "utf-8"
+//     req.setEncoding("utf-8");
+
+//     // Escuchamos el evento "data" para leer los datos de la solicitud
+//     req.on("data", (data: string) => {
+//         console.log(data); // Imprimimos los datos en la consola
+//     });
+
+//     // Escuchamos el evento "end" para indicar que se han leído todos los datos
+//     req.on("end", () => {
+//         console.log("End: all data read"); // Imprimimos un mensaje indicando que se han leído todos los datos
+//         resp.end(); // Finalizamos la respuesta
+//     });
+// };
+
+// Lectura de datos en un bucle en el archivo readHandler.ts en la carpeta src.
+import { IncomingMessage, ServerResponse } from "http";
+// Definimos la función readHandler como una función asíncrona que manejará la lectura de datos
+export const readHandler = async (
+    req: IncomingMessage,
+    resp: ServerResponse
+) => {
     // Establecemos la codificación de la solicitud como "utf-8"
     req.setEncoding("utf-8");
-    
-    // Escuchamos el evento "data" para leer los datos de la solicitud
-    req.on("data", (data: string) => {
+
+    // Iteramos de forma asíncrona sobre los datos de la solicitud
+    for await (const data of req) {
         console.log(data); // Imprimimos los datos en la consola
-    });
-    
-    // Escuchamos el evento "end" para indicar que se han leído todos los datos
-    req.on("end", () => {
-        console.log("End: all data read"); // Imprimimos un mensaje indicando que se han leído todos los datos
-        resp.end(); // Finalizamos la respuesta
-    });
+    }
+
+    console.log("End: all data read"); // Imprimimos un mensaje indicando que se han leído todos los datos
+
+    resp.end(); // Finalizamos la respuesta
 };
