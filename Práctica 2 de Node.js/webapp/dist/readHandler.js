17a3b08c26e161215bc11d4169371d3f385f25ac
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-22 15:46:39 -0600

    feat: Agregar transformador para convertir datos a minúsculas en el controlador de lectura

--- Práctica 2 de Node.js/webapp/dist/readHandler.js
+++ Práctica 2 de Node.js/webapp/dist/readHandler.js
@@ -6,8 +6,18 @@
 // }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.readHandler = void 0;
+const stream_1 = require("stream");
 // Exportamos la función readHandler que manejará la conducción de datos
 const readHandler = async (req, resp) => {
-    req.pipe(resp); // Utilizamos el método pipe para conducir los datos de la solicitud al flujo de respuesta
+    // Utilizamos el método pipe para conducir los datos de la solicitud al flujo de respuesta
+    req.pipe(createLowerTransform()).pipe(resp);
 };
 exports.readHandler = readHandler;
+// Creamos un transformador que convierte los datos a minúsculas
+const createLowerTransform = () => new stream_1.Transform({
+    // La función transform recibe los datos, la codificación y una función de devolución de llamada
+    transform(data, encoding, callback) {
+        // Convertimos los datos a minúsculas y los pasamos a través de la función de devolución de llamada
+        callback(null, data.toString().toLowerCase());
+    }
+});
--- Práctica 2 de Node.js/webapp/src/readHandler.ts
+++ Práctica 2 de Node.js/webapp/src/readHandler.ts
@@ -47,14 +47,39 @@
 
 
 
-// Conducción de datos al archivo readHandler.ts en la carpeta src.
-// El método pipe se utiliza para conectar un flujo Readable a un flujo Writeable, lo que
-// garantiza que todos los datos se lean del Readable y se escriban en el Writable sin más
-// intervención,
+// // Conducción de datos al archivo readHandler.ts en la carpeta src.
+// // El método pipe se utiliza para conectar un flujo Readable a un flujo Writeable, lo que
+// // garantiza que todos los datos se lean del Readable y se escriban en el Writable sin más
+// // intervención,
 
+// import { IncomingMessage, ServerResponse } from "http";
+
+// // Exportamos la función readHandler que manejará la conducción de datos
+// export const readHandler = async (req: IncomingMessage, resp: ServerResponse) => {
+//     req.pipe(resp); // Utilizamos el método pipe para conducir los datos de la solicitud al flujo de respuesta
+// }
+
+
+
+
+// Transformación de datos
+// La clase Transform se utiliza para crear objetos, conocidos como transformadores, que
+// reciben datos de un flujo de lectura, los procesan de alguna manera y luego los pasan. Los
+// transformadores se aplican a los flujos con el método de canalización, como se muestra en el
+// Creación de un transformador en el archivo readHandler.ts en la carpeta src.
 import { IncomingMessage, ServerResponse } from "http";
-
+import { Transform } from "stream";
 // Exportamos la función readHandler que manejará la conducción de datos
 export const readHandler = async (req: IncomingMessage, resp: ServerResponse) => {
-    req.pipe(resp); // Utilizamos el método pipe para conducir los datos de la solicitud al flujo de respuesta
+    // Utilizamos el método pipe para conducir los datos de la solicitud al flujo de respuesta
+    req.pipe(createLowerTransform()).pipe(resp);
 }
+
+// Creamos un transformador que convierte los datos a minúsculas
+const createLowerTransform = () => new Transform({
+    // La función transform recibe los datos, la codificación y una función de devolución de llamada
+    transform(data, encoding, callback) {
+        // Convertimos los datos a minúsculas y los pasamos a través de la función de devolución de llamada
+        callback(null, data.toString().toLowerCase());
+    }
+});
