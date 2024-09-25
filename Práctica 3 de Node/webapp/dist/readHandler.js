77bce69bea92bd07c49c23c99f9c1debbda5cb83
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-24 19:05:41 -0600

    feat: Transformar texto a minúsculas en el controlador de lectura

--- Práctica 3 de Node/webapp/dist/readHandler.js
+++ Práctica 3 de Node/webapp/dist/readHandler.js
@@ -4,11 +4,13 @@ exports.readHandler = void 0;
 const stream_1 = require("stream");
 // Handler para procesar la solicitud POST y transformar el texto a minúsculas
 const readHandler = async (req, resp) => {
+    // Transforma el texto a minúsculas y lo envía en la respuesta
     req.pipe(createLowerTransform()).pipe(resp);
 };
 exports.readHandler = readHandler;
 // Transformador que convierte el texto recibido a minúsculas
 const createLowerTransform = () => new stream_1.Transform({
+    // Transforma los datos a minúsculas y los envía al siguiente paso en el flujo de datos
     transform(data, encoding, callback) {
         callback(null, data.toString().toLowerCase());
     }
--- Práctica 3 de Node/webapp/src/readHandler.ts
+++ Práctica 3 de Node/webapp/src/readHandler.ts
@@ -3,11 +3,13 @@ import { Transform } from "stream";
 
 // Handler para procesar la solicitud POST y transformar el texto a minúsculas
 export const readHandler = async (req: IncomingMessage, resp: ServerResponse) => {
+    // Transforma el texto a minúsculas y lo envía en la respuesta
     req.pipe(createLowerTransform()).pipe(resp);
 };
 
 // Transformador que convierte el texto recibido a minúsculas
 const createLowerTransform = () => new Transform({
+    // Transforma los datos a minúsculas y los envía al siguiente paso en el flujo de datos
     transform(data, encoding, callback) {
         callback(null, data.toString().toLowerCase());
     }
