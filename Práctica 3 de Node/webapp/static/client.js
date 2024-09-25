69507b813c666b856e28c4d0e65250825583e56b
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-24 19:08:10 -0600

    feat: Agregar evento click al botón para llamar a la función sendReq

--- Práctica 3 de Node/webapp/static/client.js
+++ Práctica 3 de Node/webapp/static/client.js
@@ -1,20 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Agrega un evento click al botón , que llama a la función sendReq
     document.getElementById("btn").addEventListener("click", sendReq);
 });
 
+// Función que envía una solicitud POST al servidor
 const sendReq = async () => {
-    const inputText = document.getElementById("inputText").value; // Obtiene el texto del usuario
+    // Obtiene el valor del campo de texto del usuario
+    const inputText = document.getElementById("inputText").value;
 
+    // Verifica si el campo de texto está vacío
     if (inputText.trim() === "") {
         alert("Por favor ingresa un texto.");
         return;
     }
 
+    // Envía una solicitud POST al servidor con el texto ingresado por el usuario
     const response = await fetch("/read", {
         method: "POST",
         body: inputText
     });
 
+    // Actualiza el contenido de los elementos HTML con la respuesta del servidor 
     document.getElementById("msg").textContent = response.statusText;
     document.getElementById("body").textContent = await response.text();
 };
