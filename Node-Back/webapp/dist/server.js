63bfbb39720cba9869e4cf2b3e6cae7f79a0a352
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-25 07:53:20 -0600

    feat: Update server files to serve static files and add TypeScript compilation

--- Node-Back/webapp/dist/server.js
+++ Node-Back/webapp/dist/server.js
@@ -1,9 +1,20 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = require("http");
-const handler_1 = require("./handler");
-const server = (0, http_1.createServer)(handler_1.handler);
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const expressApp = (0, express_1.default)();
+const server = (0, http_1.createServer)(expressApp);
 const port = 5000;
-server.listen(port, function () {
-    console.log(`Server listening on port ${port}`);
+// Middleware para servir archivos estáticos desde la carpeta "static"
+expressApp.use(express_1.default.static(path_1.default.join(__dirname, '..', 'static'))); // ruta a ../static
+expressApp.use(express_1.default.static("node_modules/bootstrap/dist"));
+// Ruta para la raíz que redirige al index.html
+expressApp.get('/', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '..', 'static', 'index.html'));
 });
+// Inicio del servidor
+server.listen(port, () => console.log(`Servidor HTTP escuchando en el puerto ${port}`));
--- Node-Back/webapp/src/server.ts
+++ Node-Back/webapp/src/server.ts
@@ -1,8 +1,20 @@
 import { createServer } from "http";
-import { handler } from "./handler";
+import express, { Express } from "express";
+import path from 'path';
 
-const server = createServer(handler);
+const expressApp: Express = express();
+
+const server = createServer(expressApp);
 const port = 5000;
-server.listen(port, function () {
-  console.log(`Server listening on port ${port}`);
+
+// Middleware para servir archivos estáticos desde la carpeta "static"
+expressApp.use(express.static(path.join(__dirname, '..', 'static')));  // ruta a ../static
+expressApp.use(express.static("node_modules/bootstrap/dist"));
+
+// Ruta para la raíz que redirige al index.html
+expressApp.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'static', 'index.html')); 
 });
+
+// Inicio del servidor
+server.listen(port, () => console.log(`Servidor HTTP escuchando en el puerto ${port}`));
