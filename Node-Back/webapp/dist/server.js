ebcffd20bb0be73608af7dc27f82eb60e33c9f1b
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-27 10:29:18 -0600

    feat: Actualizar archivo de videojuegos con nuevas imágenes y descripciones

--- Node-Back/webapp/dist/server.js
+++ Node-Back/webapp/dist/server.js
@@ -6,15 +6,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = require("http");
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
 const expressApp = (0, express_1.default)();
 const server = (0, http_1.createServer)(expressApp);
 const port = 5000;
 // Middleware para servir archivos estáticos desde la carpeta "static"
-expressApp.use(express_1.default.static(path_1.default.join(__dirname, '..', 'static'))); // ruta a ../static
+expressApp.use(express_1.default.static(path_1.default.join(__dirname, '..', 'static')));
 expressApp.use(express_1.default.static("node_modules/bootstrap/dist"));
 // Ruta para la raíz que redirige al index.html
 expressApp.get('/', (req, res) => {
     res.sendFile(path_1.default.join(__dirname, '..', 'static', 'index.html'));
+});
+// Nueva ruta para obtener los datos del JSON
+expressApp.get('/api/videojuegos', (req, res) => {
+    const dataPath = path_1.default.join(__dirname, '..', '..', 'data', 'videojuegos.json');
+    fs_1.default.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error al leer el json');
+        }
+        else {
+            res.json(JSON.parse(data));
+        }
+    });
 });
 // Inicio del servidor
 server.listen(port, () => console.log(`Servidor HTTP escuchando en el puerto ${port}`));
--- Node-Back/webapp/src/server.ts
+++ Node-Back/webapp/src/server.ts
@@ -1,19 +1,31 @@
 import { createServer } from "http";
 import express, { Express } from "express";
 import path from 'path';
+import fs from 'fs';
 
 const expressApp: Express = express();
-
 const server = createServer(expressApp);
 const port = 5000;
 
 // Middleware para servir archivos estáticos desde la carpeta "static"
-expressApp.use(express.static(path.join(__dirname, '..', 'static')));  // ruta a ../static
+expressApp.use(express.static(path.join(__dirname, '..', 'static')));
 expressApp.use(express.static("node_modules/bootstrap/dist"));
 
 // Ruta para la raíz que redirige al index.html
 expressApp.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'static', 'index.html')); 
+  res.sendFile(path.join(__dirname, '..', 'static', 'index.html'));
+});
+
+// Nueva ruta para obtener los datos del JSON
+expressApp.get('/api/videojuegos', (req, res) => {
+  const dataPath = path.join(__dirname, '..', '..', 'data', 'videojuegos.json');
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error al leer el json');
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
 });
 
 // Inicio del servidor
