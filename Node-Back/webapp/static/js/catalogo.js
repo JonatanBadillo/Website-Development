f1f3bd6a2329d7ab3ae73517be35eed0bf72891a
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-27 19:08:15 -0600

    feat: Actualizar imágenes y descripciones de videojuegos

--- Node-Back/webapp/static/js/catalogo.js
+++ Node-Back/webapp/static/js/catalogo.js
@@ -3,25 +3,33 @@ function crearCartas(videojuegos) {
     const contenedorCartas = document.querySelector('.row');
     contenedorCartas.innerHTML = ''; // Limpiar el contenido previo
   
+    // Crear una carta por cada videojuego
     videojuegos.forEach(videojuego => {
+        // Crear elementos HTML
       const colDiv = document.createElement('div');
-      colDiv.className = 'col';
+      colDiv.className = 'col'; 
   
+      // Crear la estructura de la carta
       const cardDiv = document.createElement('div');
       cardDiv.className = 'card h-100';
   
+      // Crear la imagen del videojuego
       const img = document.createElement('img');
       img.src = videojuego.imagen;
       img.className = 'card-img-top';
       img.alt = videojuego.nombre;
   
+      // Crear el cuerpo de la carta
       const cardBody = document.createElement('div');
       cardBody.className = 'card-body';
-  
+
+      // Crear el título y la descripción del videojuego  
       const title = document.createElement('h5');
       title.className = 'card-title';
+      // Crear un enlace con el nombre del videojuego
       title.textContent = videojuego.nombre;
   
+      // Crear la descripción del videojuego
       const description = document.createElement('p');
       description.className = 'card-text';
       description.innerHTML = `<p><b>Descripción del videojuego:</b><br>${videojuego.descripcion}</p>
@@ -31,6 +39,7 @@ function crearCartas(videojuegos) {
       cardBody.appendChild(title);
       cardBody.appendChild(description);
   
+      // Crear el pie de la carta
       const cardFooter = document.createElement('div');
       cardFooter.className = 'card-footer';
   
@@ -41,6 +50,7 @@ function crearCartas(videojuegos) {
         <button type="button" class="btn btn-danger">Eliminar</button>
       `;
   
+      // Agregar los elementos al DOM
       cardFooter.appendChild(small);
   
       cardDiv.appendChild(img);
@@ -57,4 +67,3 @@ function crearCartas(videojuegos) {
     .then(response => response.json())
     .then(data => crearCartas(data))
     .catch(error => console.error('Error al cargar los datos de videojuegos:', error));
-  
