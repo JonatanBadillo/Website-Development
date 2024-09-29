c73694ad594fe459afef53344d952138c362ab71
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-09-29 14:17:27 -0600

    chore: Update server configuration to support HTTPS redirection

--- Node-Back/webapp/dist/server.js
+++ Node-Back/webapp/dist/server.js
@@ -4,12 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = require("http");
+const https_1 = require("https");
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
 const handler_1 = require("./handler"); // Importar las funciones de handler.ts
 const expressApp = (0, express_1.default)();
-const server = (0, http_1.createServer)(expressApp);
-const port = 5000;
+const httpPort = 5000; // Puerto para HTTP
+const httpsPort = 5050; // Puerto para HTTPS
+// Cargar certificados
+const privateKey = fs_1.default.readFileSync(path_1.default.join(__dirname, '..', 'key.pem'), 'utf8');
+const certificate = fs_1.default.readFileSync(path_1.default.join(__dirname, '..', 'cert.pem'), 'utf8');
+const credentials = { key: privateKey, cert: certificate };
 // Middleware para servir archivos estáticos
 expressApp.use(express_1.default.static(path_1.default.join(__dirname, '..', 'static')));
 expressApp.use(express_1.default.static("node_modules/bootstrap/dist"));
@@ -25,5 +31,25 @@ expressApp.post('/api/videojuegos', handler_1.uploadHandler, handler_1.postVideo
 expressApp.put('/api/videojuegos', handler_1.uploadHandler, handler_1.editVideojuego);
 // Ruta para eliminar un videojuego
 expressApp.delete('/api/videojuegos/:id', handler_1.deleteVideojuego);
-// Inicio del servidor
-server.listen(port, () => console.log(`Servidor HTTP escuchando en el puerto ${port}`));
+// Crear servidor HTTPS
+const httpsServer = (0, https_1.createServer)(credentials, expressApp);
+httpsServer.listen(httpsPort, () => {
+    console.log(`Servidor HTTPS escuchando en el puerto ${httpsPort}`);
+});
+// Crear servidor HTTP y redirigir a HTTPS
+const httpServer = (0, http_1.createServer)(expressApp);
+httpServer.listen(httpPort, () => {
+    console.log(`Servidor HTTP escuchando en el puerto ${httpPort}`);
+});
+// Redirigir HTTP a HTTPS
+httpServer.on('request', (req, res) => {
+    const host = req.headers.host; // Obtener el host
+    if (host) { // Verificar que host no sea undefined
+        res.writeHead(301, { Location: `https://${host.replace(httpPort.toString(), httpsPort.toString())}${req.url}` });
+        res.end(); // Aquí cerramos la respuesta inmediatamente después de enviar la redirección
+    }
+    else {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Error: Host no definido');
+    }
+});
--- Node-Back/webapp/src/server.ts
+++ Node-Back/webapp/src/server.ts
@@ -1,11 +1,18 @@
 import { createServer } from "http";
+import { createServer as createHttpsServer } from "https";
 import express, { Express } from "express";
 import path from 'path';
-import { getVideojuegos, postVideojuego, uploadHandler , editVideojuego ,deleteVideojuego } from './handler'; // Importar las funciones de handler.ts
+import fs from 'fs';
+import { getVideojuegos, postVideojuego, uploadHandler, editVideojuego, deleteVideojuego } from './handler'; // Importar las funciones de handler.ts
 
 const expressApp: Express = express();
-const server = createServer(expressApp);
-const port = 5000;
+const httpPort = 5000; // Puerto para HTTP
+const httpsPort = 5050; // Puerto para HTTPS
+
+// Cargar certificados
+const privateKey = fs.readFileSync(path.join(__dirname, '..', 'key.pem'), 'utf8');
+const certificate = fs.readFileSync(path.join(__dirname, '..', 'cert.pem'), 'utf8');
+const credentials = { key: privateKey, cert: certificate };
 
 // Middleware para servir archivos estáticos
 expressApp.use(express.static(path.join(__dirname, '..', 'static')));
@@ -28,6 +35,27 @@ expressApp.put('/api/videojuegos', uploadHandler, editVideojuego);
 // Ruta para eliminar un videojuego
 expressApp.delete('/api/videojuegos/:id', deleteVideojuego);
 
+// Crear servidor HTTPS
+const httpsServer = createHttpsServer(credentials, expressApp);
+httpsServer.listen(httpsPort, () => {
+  console.log(`Servidor HTTPS escuchando en el puerto ${httpsPort}`);
+});
 
-// Inicio del servidor
-server.listen(port, () => console.log(`Servidor HTTP escuchando en el puerto ${port}`));
+// Crear servidor HTTP y redirigir a HTTPS
+const httpServer = createServer(expressApp);
+httpServer.listen(httpPort, () => {
+  console.log(`Servidor HTTP escuchando en el puerto ${httpPort}`);
+});
+
+// Redirigir HTTP a HTTPS
+httpServer.on('request', (req, res) => {
+  const host = req.headers.host; // Obtener el host
+  if (host) { // Verificar que host no sea undefined
+    res.writeHead(301, { Location: `https://${host.replace(httpPort.toString(), httpsPort.toString())}${req.url}` });
+    res.end(); // Aquí cerramos la respuesta inmediatamente después de enviar la redirección
+  } else {
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Error: Host no definido');
+  }
+});
+
