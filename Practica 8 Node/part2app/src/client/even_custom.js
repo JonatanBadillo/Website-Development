d70f1b613bd0805d65babf278267799cc8505a13
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-10-12 15:45:12 -0600

    feat: Implementar lazy loading para imágenes en la carga de videojuegos
    - Eliminar código innecesario de la función sendReq en client.js
    - Agregar importaciones y funciones para implementar un contador personalizado en client.js
    - Modificar el archivo index.html para mostrar el contador personalizado en lugar de la tabla

---
+++ Practica 8 Node/part2app/src/client/even_custom.js
@@ -0,0 +1,6 @@
+// Exporta una función llamada Even que toma un parámetro llamado context
+export const Even = (context) => `
+<h4 class="bg-secondary text-white m-2 p-2">
+    <!-- Muestra el valor del contador si es par -->
+    Even value: ${context.counter}
+</h4>`;
