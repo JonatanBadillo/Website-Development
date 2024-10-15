c06f4b6f534a4e1cb054ee87d38abf898fbb9c0d
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-10-15 11:34:52 -0600

    index and js of client

--- Practica 8 Node/part2app/src/client/client.js
+++ Practica 8 Node/part2app/src/client/client.js
@@ -1,46 +1,50 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     document.getElementById("btn").addEventListener("click", sendReq);
-//     });
-//     sendReq = async () => {
-//     const response = await fetch("/test", {
-//     method: "POST", body: JSON.stringify({message: "Hello, World"}),
-//     headers: { "Content-Type": "application/json" }
-//     });
-//     document.getElementById("msg").textContent = response.statusText;
-//     document.getElementById("body").innerHTML = await response.text();
-//     };
+// // document.addEventListener('DOMContentLoaded', function() {
+// //     document.getElementById("btn").addEventListener("click", sendReq);
+// //     });
+// //     sendReq = async () => {
+// //     const response = await fetch("/test", {
+// //     method: "POST", body: JSON.stringify({message: "Hello, World"}),
+// //     headers: { "Content-Type": "application/json" }
+// //     });
+// //     document.getElementById("msg").textContent = response.statusText;
+// //     document.getElementById("body").innerHTML = await response.text();
+// //     };
 
-import { Counter } from "./counter_custom";
+// import { Counter } from "./counter_custom";
 
-// Contexto inicial con el contador en 0
-const context = {
-    counter: 0
-}
+// // Contexto inicial con el contador en 0
+// const context = {
+//     counter: 0
+// }
 
-// Acciones disponibles, en este caso incrementar el contador
-const actions = {
-    incrementCounter: () => {
-        context.counter++; // Incrementa el contador
-        render(); // Vuelve a renderizar la vista
-    }
-}
+// // Acciones disponibles, en este caso incrementar el contador
+// const actions = {
+//     incrementCounter: () => {
+//         context.counter++; // Incrementa el contador
+//         render(); // Vuelve a renderizar la vista
+//     }
+// }
 
-// Función para renderizar el componente Counter con el contexto actual
-const render = () => {
-    document.getElementById("target").innerHTML = Counter(context);
-}
+// // Función para renderizar el componente Counter con el contexto actual
+// const render = () => {
+//     document.getElementById("target").innerHTML = Counter(context);
+// }
 
-// Espera a que el DOM esté completamente cargado
-document.addEventListener('DOMContentLoaded', () => {
-    // Agrega un listener para detectar clics en el documento
-    document.onclick = (ev) => {
-        // Obtiene el atributo "action" del elemento clicado
-        const action = ev.target.getAttribute("action")
-        // Si existe una acción y está definida en las acciones, la ejecuta
-        if (action && actions[action]) {
-            actions[action]()
-        }
-    }
-    // Renderiza la vista inicial
-    render();
-});
+// // Espera a que el DOM esté completamente cargado
+// document.addEventListener('DOMContentLoaded', () => {
+//     // Agrega un listener para detectar clics en el documento
+//     document.onclick = (ev) => {
+//         // Obtiene el atributo "action" del elemento clicado
+//         const action = ev.target.getAttribute("action")
+//         // Si existe una acción y está definida en las acciones, la ejecuta
+//         if (action && actions[action]) {
+//             actions[action]()
+//         }
+//     }
+//     // Renderiza la vista inicial
+//     render();
+// });
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    // do nothing
+})
