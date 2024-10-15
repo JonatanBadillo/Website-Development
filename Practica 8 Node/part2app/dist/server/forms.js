509ee79c1ec180c13d9f5bdf05f48a44c811a7b6
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-10-15 12:25:28 -0600

    Refactorizar el manejo de formularios y rutas
    Este commit refactoriza el manejo de formularios y rutas en la aplicación. Se ha agregado middleware para el manejo de datos de formularios y se han definido las rutas para las solicitudes GET y POST en el formulario. Además, se ha agregado un campo de archivo en el formulario para enviar datos adjuntos.

--- Practica 8 Node/part2app/dist/server/forms.js
+++ Practica 8 Node/part2app/dist/server/forms.js
@@ -1,11 +1,30 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.registerFormRoutes = exports.registerFormMiddleware = void 0;
+const express_1 = __importDefault(require("express"));
+// permite el middleware y utiliza los datos que produce en la respuesta.
+// Los nombres y valores de los elementos de forma individuales se mostrarán en la respuesta, en lugar de la cadena codificada por URL
 const registerFormMiddleware = (app) => {
-    // no middleware yet
+    app.use(express_1.default.urlencoded({ extended: true }));
 };
 exports.registerFormMiddleware = registerFormMiddleware;
+//  Manejo de solicitudes GET 
 const registerFormRoutes = (app) => {
-    // no routes yet
+    app.get("/form", (req, resp) => {
+        for (const key in req.query) {
+            resp.write(`${key}: ${req.query[key]}\n`);
+        }
+        resp.end();
+    });
+    app.post("/form", (req, resp) => {
+        resp.write(`Content-Type: ${req.headers["content-type"]}\n`);
+        for (const key in req.body) {
+            resp.write(`${key}: ${req.body[key]} \n`);
+        }
+        resp.end();
+    });
 };
 exports.registerFormRoutes = registerFormRoutes;
--- Practica 8 Node/part2app/src/server/forms.ts
+++ Practica 8 Node/part2app/src/server/forms.ts
@@ -1,9 +1,26 @@
 import { Express } from "express";
+import express from 'express';
 
+// permite el middleware y utiliza los datos que produce en la respuesta.
+// Los nombres y valores de los elementos de forma individuales se mostrarán en la respuesta, en lugar de la cadena codificada por URL
 export const registerFormMiddleware = (app: Express) => {
-    // no middleware yet
+    app.use(express.urlencoded({extended: true}))
 }
 
+//  Manejo de solicitudes GET 
 export const registerFormRoutes = (app: Express) => {
-    // no routes yet
+    app.get("/form", (req, resp)=>{
+        for(const key in req.query){
+            resp.write(`${key}: ${req.query[key]}\n`)
+        }
+        resp.end();
+    });
+
+    app.post("/form",(req,resp)=>{
+        resp.write(`Content-Type: ${req.headers["content-type"]}\n`)
+        for(const key in req.body){
+            resp.write(`${key}: ${req.body[key]} \n`);
+        }
+        resp.end();
+    });
 }
