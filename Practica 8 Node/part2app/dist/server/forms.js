535d6c206ef945c4835a69e2c016cc0666a8283f
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-10-17 12:25:17 -0600

    Refactorizar el manejo de formularios y rutas, agregar multer y manejo de archivos en el formulario

--- Practica 8 Node/part2app/dist/server/forms.js
+++ Practica 8 Node/part2app/dist/server/forms.js
@@ -6,7 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.registerFormRoutes = exports.registerFormMiddleware = void 0;
 const express_1 = __importDefault(require("express"));
 const multer_1 = __importDefault(require("multer"));
-const sanitize_1 = require("./sanitize");
 const fileMiddleware = (0, multer_1.default)({ storage: multer_1.default.memoryStorage() });
 const registerFormMiddleware = (app) => {
     app.use(express_1.default.urlencoded({ extended: true }));
@@ -14,21 +13,18 @@ const registerFormMiddleware = (app) => {
 exports.registerFormMiddleware = registerFormMiddleware;
 const registerFormRoutes = (app) => {
     app.get("/form", (req, resp) => {
+        22;
         for (const key in req.query) {
             resp.write(`${key}: ${req.query[key]}\n`);
         }
         resp.end();
     });
     app.post("/form", fileMiddleware.single("datafile"), (req, resp) => {
-        resp.setHeader("Content-Type", "text/html");
-        for (const key in req.body) {
-            resp.write(`<div>${key}: ${(0, sanitize_1.sanitizeValue)(req.body[key])}</div>`);
-        }
-        if (req.file) {
-            resp.write(`<div>File: ${req.file.originalname}</div>`);
-            resp.write(`<div>${(0, sanitize_1.sanitizeValue)(req.file.buffer.toString())}</div>`);
-        }
-        resp.end();
+        resp.render("formData", {
+            ...req.body,
+            file: req.file,
+            fileData: req.file?.buffer.toString(),
+        });
     });
 };
 exports.registerFormRoutes = registerFormRoutes;
--- Practica 8 Node/part2app/src/server/forms.ts
+++ Practica 8 Node/part2app/src/server/forms.ts
@@ -7,22 +7,18 @@ export const registerFormMiddleware = (app: Express) => {
 };
 export const registerFormRoutes = (app: Express) => {
   app.get("/form", (req, resp) => {
+    22;
+
     for (const key in req.query) {
       resp.write(`${key}: ${req.query[key]}\n`);
     }
     resp.end();
   });
   app.post("/form", fileMiddleware.single("datafile"), (req, resp) => {
-    resp.setHeader("Content-Type", "text/html");
-    for (const key in req.body) {
-      resp.write(`<div>${key}: ${sanitizeValue(req.body[key])}</div>`);
-    }
-
-    if (req.file) {
-      resp.write(`<div>File: ${req.file.originalname}</div>`);
-      resp.write(`<div>${sanitizeValue(req.file.buffer.toString())}</div>`);
-    }
-    
-    resp.end();
+    resp.render("formData", {
+      ...req.body,
+      file: req.file,
+      fileData: req.file?.buffer.toString(),
+    });
   });
 };
