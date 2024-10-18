8776c027548e5ba283b57d42d68f61e8c3262dd8
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-10-18 10:22:22 -0600

    Refactorizar el manejo de formularios y rutas, agregar validación de formularios con validator.js

--- Practica 8 Node/part2app/dist/server/validation.js
+++ Practica 8 Node/part2app/dist/server/validation.js
@@ -1,6 +1,58 @@
 "use strict";
+// import { NextFunction, Request, Response } from "express";
+// type ValidatedRequest = Request & {
+//   validation: {
+//     results: {
+//       [key: string]: {
+//         [key: string]: boolean;
+//         valid: boolean;
+//       };
+//     };
+//     valid: boolean;
+//   };
+// };
+// export const validate = (propName: string) => {
+//   const tests: Record<string, (val: string) => boolean> = {};
+//   const handler = (req: Request, resp: Response, next: NextFunction) => {
+//     const vreq = req as ValidatedRequest;
+//     if (!vreq.validation) {
+//       vreq.validation = { results: {}, valid: true };
+//     }
+//     vreq.validation.results[propName] = { valid: true };
+//     Object.keys(tests).forEach((k) => {
+//       let valid = (vreq.validation.results[propName][k] = tests[k](
+//         req.body?.[propName]
+//       ));
+//       if (!valid) {
+//         vreq.validation.results[propName].valid = false;
+//         vreq.validation.valid = false;
+//       }
+//     });
+//     next();
+//   };
+//   handler.required = () => {
+//     tests.required = (val: string) => val?.trim().length > 0;
+//     return handler;
+//   };
+//   handler.minLength = (min: number) => {
+//     tests.minLength = (val: string) => val?.trim().length >= min;
+//     return handler;
+//   };
+//   handler.isInteger = () => {
+//     tests.isInteger = (val: string) => /^[0-9]+$/.test(val);
+//     return handler;
+//   };
+//   return handler;
+// };
+// export const getValidationResults = (req: Request) => {
+//   return (req as ValidatedRequest).validation || { valid: true };
+// };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getValidationResults = exports.validate = void 0;
+const validator_1 = __importDefault(require("validator"));
 const validate = (propName) => {
     const tests = {};
     const handler = (req, resp, next) => {
@@ -19,15 +71,15 @@ const validate = (propName) => {
         next();
     };
     handler.required = () => {
-        tests.required = (val) => val?.trim().length > 0;
+        tests.required = (val) => !validator_1.default.isEmpty(val, { ignore_whitespace: true });
         return handler;
     };
     handler.minLength = (min) => {
-        tests.minLength = (val) => val?.trim().length >= min;
+        tests.minLength = (val) => validator_1.default.isLength(val, { min });
         return handler;
     };
     handler.isInteger = () => {
-        tests.isInteger = (val) => /^[0-9]+$/.test(val);
+        tests.isInteger = (val) => validator_1.default.isInt(val);
         return handler;
     };
     return handler;
--- Practica 8 Node/part2app/src/client/client.js
+++ Practica 8 Node/part2app/src/client/client.js
@@ -45,6 +45,33 @@
 //     render();
 // });
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    // do nothing
-})
+// document.addEventListener('DOMContentLoaded', ()=>{
+//     // do nothing
+// })
+
+import validator from "validator";
+export const validate = (propName, formdata) => {
+  const val = formdata.get(propName);
+  const results = {};
+  const validationChain = {
+    get propertyName() {
+      return propName;
+    },
+    get results() {
+      return results;
+    },
+  };
+  validationChain.required = () => {
+    results.required = !validator.isEmpty(val, { ignore_whitespace: true });
+    return validationChain;
+  };
+  validationChain.minLength = (min) => {
+    results.minLength = validator.isLength(val, { min });
+    return validationChain;
+  };
+  validationChain.isInteger = () => {
+    results.isInteger = validator.isInt(val);
+    return validationChain;
+  };
+  return validationChain;
+};
