6533f81ca451a712be331a8750e3db06f9cdb639
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-10-29 11:24:43 -0600

    Refactorizar manejo de formularios y rutas, agregar consultas SQL para obtener resultados por nombre

--- Practica 8 Node/part2app/dist/server/data/sql_queries.js
+++ Practica 8 Node/part2app/dist/server/data/sql_queries.js
@@ -1,14 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.queryByNameSql = exports.queryAllSql = void 0;
-// Contiene las consultas SQL que se utilizarán en el servidor
+exports.insertResult = exports.insertCalculation = exports.insertPerson = exports.queryByNameSql = exports.queryAllSql = void 0;
 const baseSql = `
-SELECT Results.*, name, age, years, nextage FROM Results
-INNER JOIN People ON personId = People.id
+SELECT Results.*, name, age, years, nextage FROM Results INNER JOIN People ON personId = People.id
 INNER JOIN Calculations ON calculationId = Calculations.id`;
-// Consulta para obtener todos los resultados
 const endSql = `ORDER BY id DESC LIMIT $limit`;
-// Consulta para obtener todos los resultados
 exports.queryAllSql = `${baseSql} ${endSql}`;
-// Consulta para obtener resultados por nombre
 exports.queryByNameSql = `${baseSql} WHERE name = $name ${endSql}`;
+exports.insertPerson = `
+INSERT INTO People (name)
+SELECT $name
+WHERE NOT EXISTS (SELECT name FROM People WHERE name = $name)`;
+exports.insertCalculation = `
+INSERT INTO Calculations (age, years, nextage) SELECT $age, $years, $nextage
+WHERE NOT EXISTS
+(SELECT age, years, nextage FROM Calculations
+WHERE age = $age AND years = $years AND nextage = $nextage)`;
+exports.insertResult = `
+INSERT INTO Results (personId, calculationId)
+SELECT People.id as personId, Calculations.id as calculationId from People
+
+
+CROSS JOIN Calculations WHERE People.name = $name AND Calculations.age = $age
+AND Calculations.years = $years
+AND Calculations.nextage = $nextage`;
