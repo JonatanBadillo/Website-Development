04f15ec31f2251c6fe8179a63cb54b272f76f235
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-10-29 11:57:51 -0600

    Refactorizar manejo de formularios y rutas, agregar consultas SQL para obtener resultados por nombre y mejorar el manejo de transacciones en la base de datos

--- Practica 8 Node/part2app/dist/server/forms.js
+++ Practica 8 Node/part2app/dist/server/forms.js
@@ -7,32 +7,24 @@ exports.registerFormRoutes = exports.registerFormMiddleware = void 0;
 const express_1 = __importDefault(require("express"));
 const data_1 = __importDefault(require("./data"));
 const rowLimit = 10;
-// Función para registrar el middleware de formularios
 const registerFormMiddleware = (app) => {
-    // Usa el middleware de express para parsear datos urlencoded
     app.use(express_1.default.urlencoded({ extended: true }));
 };
 exports.registerFormMiddleware = registerFormMiddleware;
-// Función para registrar las rutas relacionadas con formularios
 const registerFormRoutes = (app) => {
-    // Ruta GET para renderizar el formulario
     app.get("/form", async (req, resp) => {
-        // Renderiza la vista "age" con el historial de resultados
         resp.render("age", {
             history: await data_1.default.getAllResults(rowLimit),
         });
     });
-    // Ruta POST para procesar el formulario
     app.post("/form", async (req, resp) => {
-        // Calcula la próxima edad sumando la edad actual y los años ingresados
         const nextage = Number.parseInt(req.body.age) + Number.parseInt(req.body.years);
-        // Crea el contexto para la vista con los datos del formulario y el historial de resultados por nombre
+        await data_1.default.saveResult({ ...req.body, nextage });
         const context = {
             ...req.body,
             nextage,
             history: await data_1.default.getResultsByName(req.body.name, rowLimit),
         };
-        // Renderiza la vista "age" con el contexto actualizado
         resp.render("age", context);
     });
 };
--- Practica 8 Node/part2app/src/server/forms.ts
+++ Practica 8 Node/part2app/src/server/forms.ts
@@ -2,34 +2,24 @@ import express, { Express } from "express";
 import repository from "./data";
 const rowLimit = 10;
 
-// Función para registrar el middleware de formularios
 export const registerFormMiddleware = (app: Express) => {
-  // Usa el middleware de express para parsear datos urlencoded
   app.use(express.urlencoded({ extended: true }));
 };
-
-// Función para registrar las rutas relacionadas con formularios
 export const registerFormRoutes = (app: Express) => {
-  // Ruta GET para renderizar el formulario
   app.get("/form", async (req, resp) => {
-    // Renderiza la vista "age" con el historial de resultados
     resp.render("age", {
       history: await repository.getAllResults(rowLimit),
     });
   });
-
-  // Ruta POST para procesar el formulario
   app.post("/form", async (req, resp) => {
-    // Calcula la próxima edad sumando la edad actual y los años ingresados
     const nextage =
       Number.parseInt(req.body.age) + Number.parseInt(req.body.years);
-    // Crea el contexto para la vista con los datos del formulario y el historial de resultados por nombre
+    await repository.saveResult({ ...req.body, nextage });
     const context = {
       ...req.body,
       nextage,
       history: await repository.getResultsByName(req.body.name, rowLimit),
     };
-    // Renderiza la vista "age" con el contexto actualizado
     resp.render("age", context);
   });
 };
