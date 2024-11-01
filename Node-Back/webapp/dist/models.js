a1d9479fd0d67686165dbbf3939862d161dfa8bf
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-11-01 17:52:58 -0600

    Refactorizar manejo de modelos de videojuegos

--- Node-Back/webapp/dist/models.js
+++ Node-Back/webapp/dist/models.js
@@ -1,2 +1,45 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Videojuego = void 0;
+// src/models.ts
+const sequelize_1 = require("sequelize");
+const database_1 = require("./database");
+class Videojuego extends sequelize_1.Model {
+    id;
+    nombre;
+    descripcion;
+    precio;
+    consola;
+    imagen;
+}
+exports.Videojuego = Videojuego;
+Videojuego.init({
+    id: {
+        type: sequelize_1.DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nombre: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false
+    },
+    descripcion: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false
+    },
+    precio: {
+        type: sequelize_1.DataTypes.FLOAT,
+        allowNull: false
+    },
+    consola: {
+        type: sequelize_1.DataTypes.JSON,
+        allowNull: false
+    },
+    imagen: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: true
+    }
+}, {
+    sequelize: database_1.sequelize,
+    modelName: 'Videojuego'
+});
---
+++ Node-Back/webapp/src/models.ts
@@ -0,0 +1,43 @@
+// src/models.ts
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from './database';
+
+export class Videojuego extends Model {
+    public id!: number;
+    public nombre!: string;
+    public descripcion!: string;
+    public precio!: number;
+    public consola!: string[];
+    public imagen!: string;
+}
+
+Videojuego.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    descripcion: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    precio: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    consola: {
+        type: DataTypes.JSON,
+        allowNull: false
+    },
+    imagen: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+}, {
+    sequelize,
+    modelName: 'Videojuego'
+});
