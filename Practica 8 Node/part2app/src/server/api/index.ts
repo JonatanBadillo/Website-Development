4ab78d36c9306971778993ef7206bc21e943b138
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-11-12 11:51:10 -0600

    operation.mjs

--- Practica 8 Node/part2app/src/server/api/index.ts
+++ Practica 8 Node/part2app/src/server/api/index.ts
@@ -1,4 +1,20 @@
 import { Express } from "express";
+import repository from "../data";
 export const createApi = (app: Express) => {
-// TODO - implement API
-}
+  app.get("/api/results", async (req, resp) => {
+    if (req.query.name) {
+      const data = await repository.getResultsByName(
+        req.query.name.toString(),
+        10
+      );
+      if (data.length > 0) {
+        resp.json(data);
+      } else {
+        resp.writeHead(404);
+      }
+    } else {
+      resp.json(await repository.getAllResults(10));
+    }
+    resp.end();
+  });
+};
